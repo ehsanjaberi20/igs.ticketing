526bcea20740b683871fda81c5bd16ba9ba0d9e4
EhsanJaberi <jaberi@igs.com>
2026-01-08 10:40:51 +0330

    #

--- IGS.Ticketing.UI/src/components/custom-ui/data-table/data-table.tsx
+++ IGS.Ticketing.UI/src/components/custom-ui/data-table/data-table.tsx
@@ -1,5 +1,86 @@
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow
+} from "@/components/ui/table.tsx";
+const invoices = [
+    {
+        invoice: "INV001",
+        paymentStatus: "Paid",
+        totalAmount: "$250.00",
+        paymentMethod: "Credit Card",
+    },
+    {
+        invoice: "INV002",
+        paymentStatus: "Pending",
+        totalAmount: "$150.00",
+        paymentMethod: "PayPal",
+    },
+    {
+        invoice: "INV003",
+        paymentStatus: "Unpaid",
+        totalAmount: "$350.00",
+        paymentMethod: "Bank Transfer",
+    },
+    {
+        invoice: "INV004",
+        paymentStatus: "Paid",
+        totalAmount: "$450.00",
+        paymentMethod: "Credit Card",
+    },
+    {
+        invoice: "INV005",
+        paymentStatus: "Paid",
+        totalAmount: "$550.00",
+        paymentMethod: "PayPal",
+    },
+    {
+        invoice: "INV006",
+        paymentStatus: "Pending",
+        totalAmount: "$200.00",
+        paymentMethod: "Bank Transfer",
+    },
+    {
+        invoice: "INV007",
+        paymentStatus: "Unpaid",
+        totalAmount: "$300.00",
+        paymentMethod: "Credit Card",
+    },
+]
+
 export const DataTable = ()=>{
     return (
-        <span>جدول</span>
+        <Table className='rounded-2xl mt-2'>
+            {/*<TableCaption>A list of your recent invoices.</TableCaption>*/}
+            <TableHeader className='text-center bg-amber-500'>
+                <TableRow>
+                    <TableHead className="border w-[100px] ">Invoice</TableHead>
+                    <TableHead className="border">Status</TableHead>
+                    <TableHead className="border">Method</TableHead>
+                    <TableHead className="border">Amount</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {invoices.map((invoice) => (
+                    <TableRow  dir='rtl' key={invoice.invoice}>
+                        <TableCell className="font-medium border">{invoice.invoice}</TableCell>
+                        <TableCell className='border'>{invoice.paymentStatus}</TableCell>
+                        <TableCell className='border'>{invoice.paymentMethod}</TableCell>
+                        <TableCell className="border">{invoice.totalAmount}</TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+            <TableFooter>
+                <TableRow>
+                    <TableCell colSpan={3}>Total</TableCell>
+                    <TableCell className="text-right">$2,500.00</TableCell>
+                </TableRow>
+            </TableFooter>
+        </Table>
     )
 }
